00fec34149d31109ec7bbe71bda23121dbcaffe7
hong-kyungs <hkyungs11@naver.com>
2022-09-19 01:09:49 +0900

    29. Countdown Clock - completed

--- 29. Countdown Clock/script.js
+++ 29. Countdown Clock/script.js
@@ -1,10 +1,15 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+	//clear any existing timer
+	clearInterval(countdown);
 	const now = Date.now();
 	const then = now + seconds * 1000;
 	displayTimeLeft(seconds);
+	displayEndTime(then);
 
 	countdown = setInterval(() => {
 		const secondLeft = Math.round((then - Date.now()) / 1000);
@@ -31,3 +36,29 @@ function displayTimeLeft(seconds) {
 	// console.log({ minutes, remainderSeconds });
 	// console.log(display);
 }
+
+function displayEndTime(timestamp) {
+	const end = new Date(timestamp);
+	const hour = end.getHours();
+	const adjustedHour = hour > 12 ? hour - 12 : hour;
+	const minutes = end.getMinutes();
+	endTime.textContent = `Be Back At ${adjustedHour}:${
+		minutes < 10 ? '0' : ''
+	}${minutes}`;
+}
+
+function startTimer() {
+	const seconds = parseInt(this.dataset.time);
+	timer(seconds);
+}
+
+buttons.forEach((button) => button.addEventListener('click', startTimer));
+
+//form 태그에 name-"customForm"을 이용해서 document.customForm.addEventListener라고 해줄 수 있다.
+document.customForm.addEventListener('submit', function (e) {
+	e.preventDefault();
+	const mins = this.minutes.value;
+	console.log(mins);
+	timer(mins * 60);
+	this.reset();
+});
